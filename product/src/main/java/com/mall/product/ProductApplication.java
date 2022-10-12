1c3329e44562b64e1840b241890fcc32764cd87a
ljl <1601029287@qq.com>
2022-10-12 13:46:08 +0800

    nacos 注册中心配置

--- product/src/main/java/com/mall/product/ProductApplication.java
+++ product/src/main/java/com/mall/product/ProductApplication.java
@@ -3,7 +3,9 @@ package com.mall.product;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @MapperScan("com.mall.product.dao")
 public class ProductApplication {
